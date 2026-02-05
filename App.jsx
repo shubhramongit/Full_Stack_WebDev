16324537e1262566c0d50330405d59e7c3075948
Shubhramjeet Sahoo <156273873+shubhramongit@users.noreply.github.com>
2026-02-05 12:25:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import React from "react";
+import soa from './assets/soa.png';
+function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <div>
+    <img src={soa} alt="app soa" />
+    <h1>Welcome to SOA Deemed to be University</h1>
+    </div>
+  ) }
+
+export default App;
