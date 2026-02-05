8022ec187115e2a801ad5b910134bd12a65f56a5
Shubhramjeet Sahoo <156273873+shubhramongit@users.noreply.github.com>
2026-02-05 12:26:33 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const cors=require("cors");
+const app=express();
+const PORT=5000;
+ 
+app.use(cors());
+app.get("/api/hello",(req,res)=>{
+    res.json({message:"Hello from Express"});
+
+});
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}/api/hello`);
+});
